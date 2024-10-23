96566458760367a53f7acd4708b646cc816fef74
Matheus Leopardi Mello Canelada Campos <ma.canelada@gmail.com>
2024-10-23 06:01:33 -0300

    Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection sucessful !!!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price'],
+  },
 });
+const Tour = mongoose.model('Tour', tourSchema);
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`App running on port ${port}...`);
+// });
